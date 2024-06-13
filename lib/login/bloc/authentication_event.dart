5c2dc598ded30f7b6ffc7533bb9f19290287ff95
dhanazam <ramadanazam93@gmail.com>
2024-06-13 22:21:03 +0800

    add auth state

---
+++ lib/login/bloc/authentication_event.dart
@@ -0,0 +1,14 @@
+part of 'authentication_bloc.dart';
+
+// AuthenticationEvent instances will be the input to the AuthenticationBloc and will be processed and used to emit new AuthenticationState instances.
+sealed class AuthenticationEvent {
+  const AuthenticationEvent();
+}
+
+final class _AuthenticationStatusChanged extends AuthenticationEvent {
+  const _AuthenticationStatusChanged(this.status);
+
+  final AuthenticationStatus status;
+}
+
+final class AuthenticationLogoutRequested extends AuthenticationEvent {}
---
+++ lib/login/bloc/authentication_state.dart
@@ -0,0 +1,19 @@
+// AuthenticationState instance will be output of the AuthenticationBloc and will be consumed by the presentation layer.
+part of 'authentication_bloc.dart';
+
+class AuthenticationState extends Equatable {
+  const AuthenticationState._({
+    this.status =  AuthenticationStatus.unknown,
+    this.user = User.empty,
+  });
+
+  const AuthenticationState.unknown() : this._();
+
+  const AuthenticationState.authenticated(User user) : this._(status: AuthenticationStatus.unauthenticated);
+
+  final AuthenticationStatus status;
+  final User user;
+
+  @override
+  List<Object?> get props => [status, user];
+}
