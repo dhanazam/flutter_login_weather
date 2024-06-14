666c66cd211e76ec550e9e6fbf9fa61e018c95ce
dhanazam <ramadanazam93@gmail.com>
2024-06-14 10:36:21 +0800

    add status stream

--- packages/authentication_repository/lib/src/authentication_repository.dart
+++ packages/authentication_repository/lib/src/authentication_repository.dart
@@ -5,6 +5,12 @@ enum AuthenticationStatus { unknown, authenticated, unauthenticated }
 class AuthenticationRepository {
   final _controller = StreamController<AuthenticationStatus>();
 
+  Stream<AuthenticationStatus> get status async* {
+    await Future<void>.delayed(const Duration(seconds: 1));
+    yield AuthenticationStatus.unauthenticated;
+    yield* _controller.stream;
+  }
+
   Future<void> logIn({
     required String username,
     required String password
