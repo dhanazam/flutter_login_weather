8221dce7e77fcf232dac75901c40dc2fd3d85784
dhanazam <ramadanazam93@gmail.com>
2024-06-14 22:32:34 +0800

    add static route

--- lib/login/view/login.dart
+++ lib/login/view/login.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
+  static Route<void> route() {
+    return MaterialPageRoute<void>(builder: (_) => const LoginPage());
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
