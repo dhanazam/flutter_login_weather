b1a45db391128247c69d2c14683f67a228a5ebd9
dhanazam <ramadanazam93@gmail.com>
2024-06-16 16:22:37 +0800

    replace default main

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:login_weather_flutter/login/view/login.dart';
+import 'package:login_weather_flutter/app.dart';
 
-void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Login Page',
-      home: LoginPage()
-    );
-  }
-}
+void main() => runApp(const App());
