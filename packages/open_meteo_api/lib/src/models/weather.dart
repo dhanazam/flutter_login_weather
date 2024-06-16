42a2698d2b063930d826888570e6331b872f4931
dhanazam <ramadanazam93@gmail.com>
2024-06-16 18:58:04 +0800

    add location & weather model

---
+++ packages/open_meteo_api/lib/src/models/weather.dart
@@ -0,0 +1,14 @@
+// Api returns
+// {
+//   "current_weather": {
+//     "temperature": 15.3,
+//     "weathercode": 63
+//   }
+// }
+
+class Weather {
+  const Weather({required this.temperature, required this.weatherCode});
+
+  final double temperature;
+  final double weatherCode;
+}
