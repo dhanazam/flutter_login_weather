9ed27662ad6f2f4ed9a52d4a3dc595ab2b995160
dhanazam <ramadanazam93@gmail.com>
2024-06-18 14:33:05 +0800

    build hydrated storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,15 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:login_weather_flutter/app.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() => runApp(const App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: kIsWeb
+        ? HydratedStorage.webStorageDirectory
+        : await getTemporaryDirectory(),
+  );
+  runApp(const App());
+}
